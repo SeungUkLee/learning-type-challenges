395bc3c70d85025ba591918fa3a791f044ae1ab3
SeungukLee <seunguk@seunguk.dev>
2021-12-30 16:55:17 +0900

    chore

--- src/medium/8-readonly-2.ts
+++ src/medium/8-readonly-2.ts
@@ -6,10 +6,6 @@ export type MyReadonly2<T, K extends keyof T> = Readonly<Pick<T, K>> & Omit<T, K
 /* Another Solution */
 // @see https://github.com/type-challenges/type-challenges/issues/6
 
-export type MyOmit<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>]: T[P]
-};
-
 /* Test Cases */
 export type Cases = [
   Expect<Alike<Test, Expected>>,
