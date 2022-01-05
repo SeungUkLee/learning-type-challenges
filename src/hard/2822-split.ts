b2b5c6ab5331d73a79ad945394a3f993655dc6be
SeungukLee <seunguk@seunguk.dev>
2022-01-05 12:18:05 +0900

    add: hard level challenges

---
+++ src/hard/2822-split.ts
@@ -0,0 +1,22 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+/* Solution */
+export type Split<S extends string, Sep extends string> = SplitIter<S, Sep, []>
+
+type SplitIter<S extends string, Sep extends string, Acc extends readonly unknown[]> =
+  S extends `${infer K}${Sep}${infer R}`
+  ? SplitIter<R, Sep, [...Acc, K]>
+  : S extends `${infer K}`
+  ? Sep extends '' ? Acc : [...Acc, K] : string[];
+
+
+/* Test Cases */
+export type Cases = [
+  Expect<Equal<Split<'Hi! How are you?', 'z'>, ['Hi! How are you?']>>,
+  Expect<Equal<Split<'Hi! How are you?', ' '>, ['Hi!', 'How', 'are', 'you?']>>,
+  Expect<Equal<Split<'Hi! How are you?', ''>, ['H', 'i', '!', ' ', 'H', 'o', 'w', ' ', 'a', 'r', 'e', ' ', 'y', 'o', 'u', '?']>>,
+  Expect<Equal<Split<'', ''>, []>>,
+  Expect<Equal<Split<'', 'z'>, ['']>>,
+  Expect<Equal<Split<string, 'whatever'>, string[]>>,
+]
+
