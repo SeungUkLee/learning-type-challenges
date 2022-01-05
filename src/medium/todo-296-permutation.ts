7422465c95acf29e64158193f95bb5db6f3f2906
SeungukLee <seunguk@seunguk.dev>
2022-01-05 13:44:19 +0900

    WIP

---
+++ src/medium/todo-296-permutation.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+/* Solution */
+// type Permutation<T extends readonly unknown[]> = ;
+
+// @see https://github.com/type-challenges/type-challenges/issues/614
+
+/* Test Cases */
+// export type Cases = [
+//   Expect<Equal<Permutation<'A'>, ['A']>>,
+//   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+//   Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+//   Expect<Equal<Permutation<never>, []>>,
+// ]
+
