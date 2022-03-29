c1ef7807a4df82f95aea5d5b4e7bdab329d8b362
SeungukLee <seunguk@seunguk.dev>
2022-03-29 15:31:29 +0900

    WIP

---
+++ src/extream/todo-5-readonly-keys.ts
@@ -0,0 +1,25 @@
+// import { Equal, Expect } from '@type-challenges/utils'
+// import { AnyObject } from '../utils'
+
+// type GetReadonlyKeys<T> = {
+
+// }
+
+// /* Test Cases */
+// export type Cases = [
+//   Expect<Equal<'title', GetReadonlyKeys<Todo1>>>,
+//   Expect<Equal<'title' | 'description', GetReadonlyKeys<Todo2>>>,
+// ]
+
+// interface Todo1 {
+//   readonly title: string
+//   description: string
+//   completed: boolean
+// }
+
+// interface Todo2 {
+//   readonly title: string
+//   readonly description: string
+//   completed?: boolean
+// }
+
