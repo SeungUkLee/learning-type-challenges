1880a46fa611c1e7bc4dbb98837f90f8d92f2439
SeungukLee <seunguk@seunguk.dev>
2022-06-23 23:04:21 +0900

    fix: 'Type instantiation is excessively deep and possibly infinite' error in 610-camelcase #3

--- src/medium/610-camelcase.ts
+++ src/medium/610-camelcase.ts
@@ -1,9 +1,35 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
 /* Solution */
-export type CamelCase<S extends string> = S extends `${infer F}-${infer R}`
-  ? R extends Capitalize<R> ? `${F}-${CamelCase<R>}` : `${F}${CamelCase<Capitalize<R>>}`
-  : S
+// export type CamelCase<S extends string> = S extends `${infer F}-${infer R}`
+//   ? R extends Capitalize<R> ? `${F}-${CamelCase<R>}` : `${F}${CamelCase<Capitalize<R>>}`
+//   : S
+
+export type CamelCase<S extends string> = CamelCaseIter<S, "">
+
+type CamelCaseIter<S extends string, Acc extends string> = S extends `${infer F}-${infer R}`
+  ? R extends Capitalize<R> ? CamelCaseIter<R, `${Acc}${F}-`> : CamelCaseIter<Capitalize<R>, `${Acc}${F}`>
+  : `${Acc}${S}`
+
+/* Split<'UpperCase'> -> Mapping Capitalize -> UnCapitalize first element */
+
+// type CamelCase2<S extends string> = Join<FirstElementUnCapitalize<MappingCapitalize<Split<S, '-'>>>, "">
+
+// type MappingCapitalize<L extends string[]> = MappingCapitalizeIter<L, []>
+// type MappingCapitalizeIter<L extends string[], Acc extends string[]> = L extends [infer F, ...infer R]
+//   ? F extends string 
+//   ? R extends string[] 
+//   ? MappingCapitalizeIter<R, [...Acc, Capitalize<F>]>
+//   : never
+//   : never
+//   : Acc
+
+// type FirstElementUnCapitalize<L extends string[]> = L extends [infer F, ...infer R]
+//   ? F extends string
+//   ? [Uncapitalize<F>, ...R]
+//   : never
+//   : never
+
 
 /* Test Cases */
 export type Cases = [
@@ -21,3 +47,4 @@ export type Cases = [
   Expect<Equal<CamelCase<'😎'>, '😎'>>,
 ]
 
+// type InfiniteCase = CamelCase<'foo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-bazfoo-bar-baz'> 
